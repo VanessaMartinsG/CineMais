4b6d9fca8ff7ae79b04fa95a0b48fb220e24768f
VanessaMartinsG <82179516+VanessaMartinsG@users.noreply.github.com>
2022-07-11 02:12:44 -0300

    Refacture: Ajuste tela de Filmes em cartaz

--- CineMais/src/FilmesEmCartazController.java
+++ CineMais/src/FilmesEmCartazController.java
@@ -2,6 +2,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -51,13 +52,23 @@ public class FilmesEmCartazController {
     @FXML
     private Label infoSalaTipo06;
     // Add references to the controls in Layout2.fxml
+
+    @FXML
+    private Button btnBuscarData;
+
+    @FXML
+    private TextField inputDia;
+
+    @FXML
+    private TextField inputMes;
+
     @FXML
     private Label lblFromController1;
     Bilheteria bilheteria = Bilheteria.getInstance();
 
     @FXML
     void irIngresso01(ActionEvent event) throws IOException {
-        
+
     }
 
     @FXML
@@ -113,64 +124,81 @@ public class FilmesEmCartazController {
     }
 
     @FXML
-    void initialize(){
+    void initialize() {
         List<Sessao> sessoes = new ArrayList<>();
         titleShopping.setText(bilheteria.getShoppingSelecionado());
-        
-        switch(bilheteria.getShoppingSelecionado()){
+
+        switch (bilheteria.getShoppingSelecionado()) {
             case "Shopping Salvador":
-            sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08",1);
-            filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
-            filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
-            filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
-            btn01Filme1.setText(sessoes.get(0).getHorario());
-            btn02Filme1.setText(sessoes.get(1).getHorario());
-            btn03Filme2.setText(sessoes.get(2).getHorario());
-            btn04Filme2.setText(sessoes.get(3).getHorario());
-            btn05Filme3.setText(sessoes.get(4).getHorario());
-            btn06Filme3.setText(sessoes.get(5).getHorario());
-            setDataTipoSala(sessoes);
-            
-            break;
+                sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08", 1);
+                filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
+                filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
+                filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
+                btn01Filme1.setText(sessoes.get(0).getHorario());
+                btn02Filme1.setText(sessoes.get(1).getHorario());
+                btn03Filme2.setText(sessoes.get(2).getHorario());
+                btn04Filme2.setText(sessoes.get(3).getHorario());
+                btn05Filme3.setText(sessoes.get(4).getHorario());
+                btn06Filme3.setText(sessoes.get(5).getHorario());
+                setDataTipoSala(sessoes);
+
+                break;
             case "Shopping Barra":
-            sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08",2);
-            filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
-            filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
-            filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
-            break;
+                sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08", 2);
+                filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
+                filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
+                filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
+                break;
             case "Shopping Paralela":
-            sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08",3);
-            filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
-            filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
-            filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
-            break;
+                sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08", 3);
+                filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
+                filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
+                filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
+                break;
             case "Shopping da Bahia":
-            sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08",4);
-            filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
-            filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
-            filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
-            break;
+                sessoes = bilheteria.getSessaoByDateAndShop("2022-07-08", 4);
+                filme1.setText(sessoes.get(0).getFilme().getNomeFilme());
+                filme2.setText(sessoes.get(2).getFilme().getNomeFilme());
+                filme3.setText(sessoes.get(4).getFilme().getNomeFilme());
+                break;
         }
     }
 
-    public void setDataTipoSala(List<Sessao> sessoes){
-        if(sessoes.get(0).getSala().isSala3d()) infoSalaTipo01.setText("Sala:"+sessoes.get(0).getSala().getNumero()+ "  " + "3D");
-            else infoSalaTipo01.setText("Sala:"+sessoes.get(0).getSala().getNumero()+ "  " + "2D");
-            
-            if(sessoes.get(1).getSala().isSala3d()) infoSalaTipo02.setText("Sala:"+sessoes.get(1).getSala().getNumero()+ "  " + "3D");
-            else infoSalaTipo02.setText("Sala:"+sessoes.get(1).getSala().getNumero()+ "  " + "2D");
-            
-            if(sessoes.get(2).getSala().isSala3d()) infoSalaTipo03.setText("Sala:"+sessoes.get(2).getSala().getNumero()+ "  " + "3D");
-            else infoSalaTipo03.setText("Sala:"+sessoes.get(2).getSala().getNumero()+ "  " + "2D");
-            
-            if(sessoes.get(3).getSala().isSala3d()) infoSalaTipo04.setText("Sala:"+sessoes.get(3).getSala().getNumero()+ "  " + "3D");
-            else infoSalaTipo04.setText("Sala:"+sessoes.get(3).getSala().getNumero()+ "  " + "2D");
-            
-            if(sessoes.get(4).getSala().isSala3d()) infoSalaTipo05.setText("Sala:"+sessoes.get(4).getSala().getNumero()+ "  " + "3D");
-            else infoSalaTipo05.setText("Sala:"+sessoes.get(4).getSala().getNumero()+ "  " + "2D");
-            
-            if(sessoes.get(5).getSala().isSala3d()) infoSalaTipo06.setText("Sala:"+sessoes.get(5).getSala().getNumero()+ "  " + "3D");
-            else infoSalaTipo06.setText("Sala:"+sessoes.get(5).getSala().getNumero()+ "  " + "2D");
+    public void setDataTipoSala(List<Sessao> sessoes) {
+        if (sessoes.get(0).getSala().isSala3d())
+            infoSalaTipo01.setText("Sala:" + sessoes.get(0).getSala().getNumero() + "  " + "3D");
+        else
+            infoSalaTipo01.setText("Sala:" + sessoes.get(0).getSala().getNumero() + "  " + "2D");
+
+        if (sessoes.get(1).getSala().isSala3d())
+            infoSalaTipo02.setText("Sala:" + sessoes.get(1).getSala().getNumero() + "  " + "3D");
+        else
+            infoSalaTipo02.setText("Sala:" + sessoes.get(1).getSala().getNumero() + "  " + "2D");
+
+        if (sessoes.get(2).getSala().isSala3d())
+            infoSalaTipo03.setText("Sala:" + sessoes.get(2).getSala().getNumero() + "  " + "3D");
+        else
+            infoSalaTipo03.setText("Sala:" + sessoes.get(2).getSala().getNumero() + "  " + "2D");
+
+        if (sessoes.get(3).getSala().isSala3d())
+            infoSalaTipo04.setText("Sala:" + sessoes.get(3).getSala().getNumero() + "  " + "3D");
+        else
+            infoSalaTipo04.setText("Sala:" + sessoes.get(3).getSala().getNumero() + "  " + "2D");
+
+        if (sessoes.get(4).getSala().isSala3d())
+            infoSalaTipo05.setText("Sala:" + sessoes.get(4).getSala().getNumero() + "  " + "3D");
+        else
+            infoSalaTipo05.setText("Sala:" + sessoes.get(4).getSala().getNumero() + "  " + "2D");
+
+        if (sessoes.get(5).getSala().isSala3d())
+            infoSalaTipo06.setText("Sala:" + sessoes.get(5).getSala().getNumero() + "  " + "3D");
+        else
+            infoSalaTipo06.setText("Sala:" + sessoes.get(5).getSala().getNumero() + "  " + "2D");
     }
+
+    // @FXML
+    // void procurarPorData(ActionEvent event) {
+
+    // }
 
 }
