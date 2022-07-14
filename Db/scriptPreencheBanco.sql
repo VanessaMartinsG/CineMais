a55543e1920028df0ca426cbfedfaec1bc29f391
unknown <aroncaiua@gmail.com>
2022-07-13 23:21:23 -0300

    novo arquivo db

--- Db/scriptPreencheBanco.sql
+++ Db/scriptPreencheBanco.sql
@@ -48,18 +48,18 @@ insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (3,"15:0
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (4,"19:30",2,3,"2022-07-08",1);
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (5,"19:30",3,1,"2022-07-08",1);
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (6,"19:30",3,2,"2022-07-08",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (7,"14:30",1,1,"2022-07-09",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (8,"16:30",1,2,"2022-07-09",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (9,"15:00",2,3,"2022-07-09",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (10,"19:30",2,3,"2022-07-09",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (11,"19:30",3,1,"2022-07-09",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (12,"19:30",3,2,"2022-07-09",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (13,"14:30",1,1,"2022-07-10",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (14,"16:30",1,2,"2022-07-10",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (15,"15:00",2,3,"2022-07-10",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (16,"19:30",2,3,"2022-07-10",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (17,"19:30",3,1,"2022-07-10",1);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (18,"19:30",3,2,"2022-07-10",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (7,"12:30",1,1,"2022-07-09",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (8,"17:30",1,2,"2022-07-09",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (9,"14:00",2,3,"2022-07-09",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (10,"20:30",2,3,"2022-07-09",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (11,"20:30",3,1,"2022-07-09",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (12,"21:30",3,2,"2022-07-09",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (13,"18:30",1,1,"2022-07-10",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (14,"15:30",1,2,"2022-07-10",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (15,"16:00",2,3,"2022-07-10",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (16,"22:30",2,3,"2022-07-10",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (17,"13:30",3,1,"2022-07-10",1);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (18,"20:30",3,2,"2022-07-10",1);
 
 
 /* Aqui sao sessoes para o shopping barra - salas c id 4 5 6*/
@@ -69,18 +69,18 @@ insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (21,"16:
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (22,"20:30",5,6,"2022-07-08",2);
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (23,"22:30",6,4,"2022-07-08",2);
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (24,"18:30",6,5,"2022-07-08",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (25,"15:30",4,4,"2022-07-09",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (25,"16:30",4,4,"2022-07-09",2);
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (26,"17:30",4,5,"2022-07-09",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (27,"16:00",5,6,"2022-07-09",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (28,"20:30",5,6,"2022-07-09",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (29,"22:30",6,4,"2022-07-09",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (30,"18:30",6,5,"2022-07-09",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (31,"15:30",4,4,"2022-07-10",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (27,"15:00",5,6,"2022-07-09",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (28,"22:30",5,6,"2022-07-09",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (29,"23:30",6,4,"2022-07-09",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (30,"19:30",6,5,"2022-07-09",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (31,"21:30",4,4,"2022-07-10",2);
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (32,"17:30",4,5,"2022-07-10",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (33,"16:00",5,6,"2022-07-10",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (34,"20:30",5,6,"2022-07-10",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (35,"22:30",6,4,"2022-07-10",2);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (36,"18:30",6,5,"2022-07-10",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (33,"14:00",5,6,"2022-07-10",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (34,"19:30",5,6,"2022-07-10",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (35,"23:30",6,4,"2022-07-10",2);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (36,"17:30",6,5,"2022-07-10",2);
 
 /* Aqui sao sessoes para o shopping paralela - salas c id 7 8 9*/
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (37,"14:30",1,7,"2022-07-08",3);
@@ -103,21 +103,21 @@ insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (53,"19:
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (54,"19:30",5,8,"2022-07-10",3);
 
 /* Aqui sao sessoes para o shopping da bahia - salas c id 10 11 12*/
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (55,"14:30",1,1,"2022-07-08",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (56,"16:30",1,2,"2022-07-08",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (57,"15:00",2,3,"2022-07-08",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (58,"19:30",2,3,"2022-07-08",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (59,"19:30",3,1,"2022-07-08",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (60,"19:30",3,2,"2022-07-08",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (55,"15:30",1,1,"2022-07-08",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (56,"17:30",1,2,"2022-07-08",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (57,"17:00",2,3,"2022-07-08",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (58,"18:30",2,3,"2022-07-08",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (59,"21:30",3,1,"2022-07-08",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (60,"23:30",3,2,"2022-07-08",4);
 insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (61,"14:30",1,7,"2022-07-09",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (62,"16:30",1,8,"2022-07-09",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (63,"15:00",4,9,"2022-07-09",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (64,"19:30",4,9,"2022-07-09",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (65,"19:30",5,7,"2022-07-09",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (66,"19:30",5,8,"2022-07-09",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (67,"14:30",1,7,"2022-07-10",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (68,"16:30",1,8,"2022-07-10",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (69,"15:00",4,9,"2022-07-10",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (70,"19:30",4,9,"2022-07-10",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (71,"19:30",5,7,"2022-07-10",4);
-insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (72,"19:30",5,8,"2022-07-10",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (62,"13:30",1,8,"2022-07-09",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (63,"20:00",4,9,"2022-07-09",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (64,"21:30",4,9,"2022-07-09",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (65,"18:30",5,7,"2022-07-09",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (66,"17:30",5,8,"2022-07-09",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (67,"13:30",1,7,"2022-07-10",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (68,"10:30",1,8,"2022-07-10",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (69,"21:00",4,9,"2022-07-10",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (70,"22:30",4,9,"2022-07-10",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (71,"16:30",5,7,"2022-07-10",4);
+insert into sessao (numero,horario,filmeId,salaId,data,cinemaId) values (72,"17:30",5,8,"2022-07-10",4);
