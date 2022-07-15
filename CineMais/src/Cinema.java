40fd4cb55e85e70f392af274d35d992ae23b44c8
VanessaMartinsG <82179516+VanessaMartinsG@users.noreply.github.com>
2022-07-15 02:36:27 -0300

    Debug: Login / Cadastro

---
+++ CineMais/src/Cinema.java
@@ -0,0 +1,27 @@
+public class Cinema {
+
+    private String nome;
+    private String shopping;
+    
+    public Cinema(String nome, String shopping) {
+        this.nome = nome;
+        this.shopping = shopping;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getShopping() {
+        return shopping;
+    }
+
+    public void setShopping(String shopping) {
+        this.shopping = shopping;
+    }
+
+}
