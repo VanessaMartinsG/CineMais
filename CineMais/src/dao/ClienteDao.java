34e69f407cd8b8e806666b6d61e6692f8b95f86b
VanessaMartinsG <82179516+VanessaMartinsG@users.noreply.github.com>
2022-07-15 02:19:14 -0300

    Feat: Validação do login / cadastro

--- CineMais/src/dao/ClienteDao.java
+++ CineMais/src/dao/ClienteDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Cliente;
 
@@ -11,11 +12,11 @@ public class ClienteDao {
     private Connection conn;
     private final String Table = "cliente";
 
-    public ClienteDao(){
+    public ClienteDao() {
         this.conn = DataBase.getInstance().initDb();
     }
 
-    public Boolean selectAndLogin(String nome, String senha){
+    public Boolean selectAndLogin(String nome, String senha) {
         PreparedStatement stmt = null;
         ResultSet rs = null;
 
@@ -24,7 +25,6 @@ public class ClienteDao {
             stmt.setString(1, nome);
             stmt.setString(1, senha);
             rs = stmt.executeQuery();
-
 
             this.conn.close();
             stmt.close();
@@ -37,7 +37,7 @@ public class ClienteDao {
 
     public boolean insert(Cliente cliente) {
         PreparedStatement stmt = null;
-        //int tipoEstudante = (cliente instanceof Estudante) ? 1 : 0;
+        // int tipoEstudante = (cliente instanceof Estudante) ? 1 : 0;
         try {
             // Passagem de parametros
             stmt = this.conn.prepareStatement(
@@ -49,8 +49,6 @@ public class ClienteDao {
             stmt.setInt(3, cliente.getIdade());
             stmt.setString(4, cliente.getEmail());
             stmt.setString(5, cliente.getSenha());
-            
-            
 
             // Execução da SQL
             stmt.executeUpdate();
@@ -69,20 +67,48 @@ public class ClienteDao {
         PreparedStatement stmt = null;
         ResultSet rs = null;
 
-
         try {
             stmt = this.conn.prepareStatement("SELECT * FROM " + this.Table + " WHERE email like ?");
             stmt.setString(1, cliente.getEmail());
             rs = stmt.executeQuery();
 
             while (rs.next()) {
-        
 
                 cliente.setNome(rs.getString("nome"));
                 cliente.setEstudante(rs.getBoolean("estudante"));
                 cliente.setIdade(rs.getInt("idade"));
                 cliente.setEmail(rs.getString("email"));
                 cliente.setSenha(rs.getString("senha"));
+            }
+
+            this.conn.close();
+            stmt.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return cliente;
+    }
+
+    public ArrayList<Cliente> selectAll() {
+        ArrayList<Cliente> cliente = new ArrayList<Cliente>();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = this.conn.prepareStatement("SELECT * FROM " + this.Table);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                Cliente cadastro = new Cliente();
+                cadastro.setNome(rs.getString("nome"));
+                cadastro.setEstudante(rs.getBoolean("estudante"));
+                cadastro.setIdade(rs.getInt("idade"));
+                cadastro.setEmail(rs.getString("email"));
+                cadastro.setSenha(rs.getString("senha"));
+                cliente.add(cadastro);
             }
 
             this.conn.close();
